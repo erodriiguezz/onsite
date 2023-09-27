af824e90464c4a8ea0c5afcf7b6bb43bb6bf3864
Ernesto Rodriguez <erodriiguezz@icloud.com>
2023-09-26 22:01:09 -0400

    moved router to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Login from "./components/Login";
+
+const router = createBrowserRouter([
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <>Register</> },
+  { path: "/dashboard", element: <>Dashboard</> },
+]);
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import { AuthProvider } from "./context/AuthProvider";
 import App from "./App.jsx";
-import Login from "./components/Login";
-
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/login", element: <Login /> },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
